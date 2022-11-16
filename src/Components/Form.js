c463c68d35d36f22b0a6d0d050615e034b7bbf70
Stephanie Hill <stephni.hill@gmail.com>
2022-11-15 20:01:05 -0800

    table attempts

---
+++ src/Components/Form.js
@@ -0,0 +1,42 @@
+// import { Link } from 'gatsby'
+// import React, { useState } from 'react'
+// // import Layout from '../Components/Layout'
+
+
+// const FormPage = (props) => {
+
+//   const [city, setCity] = useState('');
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//   }
+
+//   // const handleChange = (event) => {
+//   //   setCity({value: event.target.value});
+//   // }
+
+//   return (
+
+//       // <Layout pageTitle="Form">
+//         <main>
+//         {/* <Link to="/">Home</Link> */}
+//         <form onSubmit={handleSubmit}>
+//           <label htmlfFor="city">City:</label>
+//           <input
+//             id="city"
+//             type="text"
+//             value={city}
+//             onChange={(e) => setCity(e.target.value)} />
+//           <p>{city}</p>
+//           <button type="submit">SUBMIT</button>
+//         </form>
+//         </main>
+//       // </Layout>
+
+
+//   )
+// }
+
+// export default FormPage
+
+// export const Head = () => <title>Form</title>
